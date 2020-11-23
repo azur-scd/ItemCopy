783019158d3265aff0b1303429abb267f8899583
Daniel Berthereau <Daniel.git@Berthereau.net>
2020-11-23 00:00:00 +0000

    Deferred js.

--- Module.php
+++ Module.php
@@ -6,22 +6,22 @@ use Laminas\EventManager\SharedEventManagerInterface;
 use Omeka\Module\AbstractModule;
 
 /**
- * ItemCopy
- */
-/**
  * The ItemCopy plugin.
  */
 class Module extends AbstractModule
 {
     public function attachListeners(SharedEventManagerInterface $sharedEventManager): void
     {
-        $sharedEventManager->attach('Omeka\Controller\Admin\Item',
-            'view.browse.after', [$this, 'addItemCopyJs']);
+        $sharedEventManager->attach(
+            'Omeka\Controller\Admin\Item',
+            'view.browse.after',
+            [$this, 'addItemCopyJs']
+        );
     }
 
     public function addItemCopyJs(Event $event): void
     {
         $view = $event->getTarget();
-        $view->headScript()->appendFile($view->assetUrl('item-copy.js', 'ItemCopy'));
+        $view->headScript()->appendFile($view->assetUrl('item-copy.js', 'ItemCopy'), 'text/javascript', ['defer' => 'defer']);
     }
 }
